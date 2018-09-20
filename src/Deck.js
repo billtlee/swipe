8a6e71b5c3cec594ae997a34fc4294e5bac06f4f
billtlee <wlee3@hotmail.com>
2018-09-20 11:48:15 +0800

    renderCards to render next card after swipe

--- src/Deck.js
+++ src/Deck.js
@@ -39,6 +39,8 @@ class Deck extends Component {
     const item = data[this.state.index];
 
     direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    this.state.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1 });
   }
 
   getCardStyle() {
@@ -69,8 +71,10 @@ class Deck extends Component {
   }
 
   renderCards() {
-    return this.props.data.map((item, index) => {
-      if (index === 0) {
+    return this.props.data.map((item, i) => {
+      if (i < this.state.index) { return null; }
+      
+      if (i === this.state.index) {
         return (
           <Animated.View
             key={item.id}
